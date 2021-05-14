17c1e88564f4d79444ac5ea1e54c9598ceace62d
ct-clmsn <ct.clmsn@gmail.com>
2021-05-14 00:08:06 -0400

    Update lib.rs
    Fixed type-o

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//  Cloneright (c) 2021 Christopher Taylor
+//  Copyright (c) 2021 Christopher Taylor
 //
 //  SPDX-License-Identifier: BSL-1.0
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
